253f8ee565416faf5a529e7bcdb5c8bcec267261
Chukwudi Eze <chukwudieze97@gmail.com>
2025-10-04 13:00:46 +0100

    fix build

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,6 +5,12 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 import { FaSpinner } from "react-icons/fa"
 
+export type ButtonProps = React.ComponentProps<"button"> &
+VariantProps<typeof buttonVariants> & {
+  asChild?: boolean;
+  isLoading?: boolean;
+}
+
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 cursor-pointer whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
@@ -45,11 +51,7 @@ function Button({
   isLoading,
   children,
   ...props
-}: React.ComponentProps<"button"> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean;
-    isLoading?: boolean;
-  }) {
+}: ButtonProps) {
   const Comp = asChild ? Slot : "button"
 
   return (
